748bdec3990ab047b0c5b83cb8559ad60c99f1ca
ttokutake <tadatoshi.tokutake@gmail.com>
2015-03-22 19:30:53 +0900

    Delete associative_zip() because of existing array_combine()

--- ArrayTest.php
+++ ArrayTest.php
@@ -4,14 +4,6 @@ require_once('array.php');
 
 class ArrayTest extends PHPUnit_Framework_TestCase
 {
-   public function testAssociativeZip()
-   {
-      $keys           = [     'one',      'two',      'three'];
-      $values         = [         1,          2,            3];
-      $expected_array = ['one' => 1, 'two' => 2, 'three' => 3];
-      $this->assertEquals($expected_array, associative_zip($keys, $values));
-   }
-
    private $array             = [1, 2, 3, 4, 5];
    private $associative_array = ['one' => 1, 'two' => 2, 'three' => 3];
 
--- array.php
+++ array.php
@@ -1,32 +1,23 @@
 <?php
 
 /**
- * Create an associative array from pair arrays of keys and values.
- * Length of The associative array is shorter length of pair arrays.
+ * Return the pair of first element and the others of an array.
  *
- * @param  array $keys
- * @param  array $values
- * @return array
+ * @param  array $array
+ * @return array [$head, $tail]
  */
-function associative_zip(array $keys, array $values)
-{
-   $formatted_keys   = array_values($keys);
-   $formatted_values = array_values($values);
-
-   $associative_array = [];
-   $min_length        = min(count($formatted_keys), count($formatted_values));
-   for ($i = 0; $i < $min_length; $i++) {
-      $associative_array[$formatted_keys[$i]] = $formatted_values[$i];
-   }
-   return $associative_array;
-}
-
 function array_behead(array $array)
 {
    $head = array_shift($array);
    return [$head, $array];
 }
 
+/**
+ * Return the pair of last element and the others of an array.
+ *
+ * @param  array $array
+ * @return array [$init, $last]
+ */
 function array_depeditate(array $array)
 {
    $last = array_pop($array);
