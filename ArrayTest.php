db2a9a9139513cd8fbd27adfb6771f3523b93185
ttokutake <tadatoshi.tokutake@gmail.com>
2015-03-22 22:28:42 +0900

    create array_zip() and enhance ArrayTest.php

--- ArrayTest.php
+++ ArrayTest.php
@@ -4,18 +4,77 @@ require_once('array.php');
 
 class ArrayTest extends PHPUnit_Framework_TestCase
 {
-   private $array             = [1, 2, 3, 4, 5];
-   private $associative_array = ['one' => 1, 'two' => 2, 'three' => 3];
+   private $arrays = [
+      [],
 
-   public function testArrayBehead()
+      [1      ],
+      [1, 2   ],
+      [1, 2, 3],
+
+      ['one' => 1                          ],
+      ['one' => 1, 'two' => 2              ],
+      ['one' => 1, 'two' => 2, 'three' => 3],
+   ];
+
+   public function testArrayZipForArrayBehead()
    {
-      $this->assertEquals([1, [2, 3, 4, 5]]              , array_behead($this->array));
-      $this->assertEquals([1, ['two' => 2, 'three' => 3]], array_behead($this->associative_array));
+      $next_expected = [
+         [null, []],
+
+         [1, [    ]],
+         [1, [2   ]],
+         [1, [2, 3]],
+
+         [1, [                        ]],
+         [1, ['two' => 2              ]],
+         [1, ['two' => 2, 'three' => 3]],
+      ];
+
+      $patterns = array_zip($next_expected, $this->arrays);
+      $expected = array_map(function ($e1, $e2) { return [$e1, $e2]; }, $next_expected, $this->arrays);
+
+      $this->assertEquals($expected, $patterns);
+
+      return $patterns;
+   }
+   /**
+    * @depends testArrayZipForArrayBehead
+    */
+   public function testArrayBehead($patterns)
+   {
+      foreach ($patterns as list($expected, $arrays)) {
+         $this->assertEquals($expected, array_behead($arrays));
+      }
    }
 
-   public function testArrayDepeditate()
+   public function testArrayZipForArrayDepeditate()
    {
-      $this->assertEquals([[1, 2, 3, 4], 5]            , array_depeditate($this->array));
-      $this->assertEquals([['one' => 1, 'two' => 2], 3], array_depeditate($this->associative_array));
+      $next_expected = [
+         [[], null],
+
+         [[    ], 1],
+         [[1   ], 2],
+         [[1, 2], 3],
+
+         [[                      ], 1],
+         [['one' => 1            ], 2],
+         [['one' => 1, 'two' => 2], 3],
+      ];
+
+      $patterns = array_zip($next_expected, $this->arrays);
+      $expected = array_map(function ($e1, $e2) { return [$e1, $e2]; }, $next_expected, $this->arrays);
+
+      $this->assertEquals($expected, $patterns);
+
+      return $patterns;
+   }
+   /**
+    * @depends testArrayZipForArrayDepeditate
+    */
+   public function testArrayDepeditate($patterns)
+   {
+      foreach ($patterns as list($expected, $arrays)) {
+         $this->assertEquals($expected, array_depeditate($arrays));
+      }
    }
 }
--- array.php
+++ array.php
@@ -1,6 +1,36 @@
 <?php
 
 /**
+ * Return an array consisting of the pairs of each element for two arrays.
+ *
+ * @param  array $array1,$array2,...
+ * @return array [[$element1_1,$element2_1,...],...]
+ */
+function array_zip()
+{
+   $arrays = func_get_args();
+   foreach ($arrays as $array) {
+      if (!is_array($array)) {
+         throw new UnexpectedValueException(__FUNCTION__ . ': Each argment must be "array" type.');
+      }
+   }
+
+   $formatted_arrays = array_map(function ($array) { return array_values($array); }, $arrays);
+   $min_length       = min(array_map(function ($array) { return count($array); }, $formatted_arrays));
+
+   $zipped_array = [];
+   for ($i = 0; $i < $min_length; $i++) {
+      $zipped_array[] = array_reduce($formatted_arrays, function ($carry, $array) use($i) {
+            $carry[] = $array[$i];
+            return $carry;
+         }, []
+      );
+   }
+
+   return $zipped_array;
+}
+
+/**
  * Return the pair of first element and the others of an array.
  *
  * @param  array $array
