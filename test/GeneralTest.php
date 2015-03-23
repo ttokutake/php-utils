86598fc2503a4cebd5ca22e350f49b85e3a2af77
ttokutake <tadatoshi.tokutake@gmail.com>
2015-03-24 00:14:44 +0900

    Just replace the position of between()

--- test/GeneralTest.php
+++ test/GeneralTest.php
@@ -31,25 +31,6 @@ class GeneralTest extends PHPUnit_Framework_TestCase
       $this->assertNull(get_or_null($undefined_var));
    }
 
-   public function testBetween()
-   {
-      $patterns = [
-         [true , 0,  0,  5],
-         [true , 0, -5,  0],
-         [true , 0, -5,  5],
-         [false, 0,  1,  5],
-         [false, 0, -5, -1],
-         [false, 0,  5, -5],
-      ];
-      foreach ($patterns as list($expected, $num, $min, $max)) {
-         $this->assertEquals($expected, between($num, $min, $max));
-      }
-
-      $iso8601 = '1987-04-20 00:00:00';
-      $this->assertTrue (between($iso8601, '1987-04-01 00:00:00', '1987-04-30 00:00:00'));
-      $this->assertFalse(between($iso8601, '1987-05-01 00:00:00', '1987-05-31 00:00:00'));
-   }
-
 
    private $odds  = [1, 3, 5];
    private $evens = [0, 2, 4];
@@ -74,5 +55,24 @@ class GeneralTest extends PHPUnit_Framework_TestCase
       foreach ($this->odds as $odd) {
          $this->assertFalse(is_even($odd));
       }
+   }
+
+   public function testBetween()
+   {
+      $patterns = [
+         [true , 0,  0,  5],
+         [true , 0, -5,  0],
+         [true , 0, -5,  5],
+         [false, 0,  1,  5],
+         [false, 0, -5, -1],
+         [false, 0,  5, -5],
+      ];
+      foreach ($patterns as list($expected, $num, $min, $max)) {
+         $this->assertEquals($expected, between($num, $min, $max));
+      }
+
+      $iso8601 = '1987-04-20 00:00:00';
+      $this->assertTrue (between($iso8601, '1987-04-01 00:00:00', '1987-04-30 00:00:00'));
+      $this->assertFalse(between($iso8601, '1987-05-01 00:00:00', '1987-05-31 00:00:00'));
    }
 }
