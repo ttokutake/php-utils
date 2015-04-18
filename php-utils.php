619de6a82847454f6903f62fe34a03815ac16a19
ttokutake <tadatoshi.tokutake@gmail.com>
2015-04-18 22:54:43 +0900

    Move require_relative() to general.inc

--- php-utils.php
+++ php-utils.php
@@ -20,25 +20,6 @@ function create_path(array $dirs)
    return array_reduce($dirs, function ($path, $dir) { return $path . $dir . DIRECTORY_SEPARATOR; }, '');
 }
 
-/**
- * Run "require_once" that is equal to "require_once __DIR__ . $path_to_file;".
- * But global variables can't be created.
- *
- * @param  string|array $path_to_file
- */
-function require_relative($path_to_file)
-{
-   $backtrace = debug_backtrace();
-   $first     = reset($backtrace);
-   $base      = dirname($first['file']);
-
-   if (is_array($path_to_file)) {
-      $path_to_file = implode(DIRECTORY_SEPARATOR, $path_to_file);
-   }
-
-   require_once $base . DIRECTORY_SEPARATOR . $path_to_file;
-}
-
 define('PATH_TO_LIB_FOR_PHP_UTILS', create_path(array(__DIR__, 'lib')));
 
 require_once PATH_TO_LIB_FOR_PHP_UTILS . 'general.inc'       ;
