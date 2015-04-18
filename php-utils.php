364819c9bdbd8ac4ca517107681834b57c1a0a0f
ttokutake <tadatoshi.tokutake@gmail.com>
2015-04-18 22:15:09 +0900

    Create require_relative()

--- php-utils.php
+++ php-utils.php
@@ -9,9 +9,34 @@
  */
 
 
+/**
+ * Return the path with DIRECTORY_SEPARATOR.
+ *
+ * @param  array $dirs
+ * @return string
+ */
 function create_path(array $dirs)
 {
    return array_reduce($dirs, function ($path, $dir) { return $path . $dir . DIRECTORY_SEPARATOR; }, '');
+}
+
+/**
+ * Run "require_once" that is equal to "require_once __DIR__ . $path_to_file;".
+ * But global variables can't be created.
+ *
+ * @param  string|array $path_to_file
+ */
+function require_relative($path_to_file)
+{
+   $backtrace = debug_backtrace();
+   $first     = reset($backtrace);
+   $base      = dirname($first['file']);
+
+   if (is_array($path_to_file)) {
+      $path_to_file = implode(DIRECTORY_SEPARATOR, $path_to_file);
+   }
+
+   require_once $base . DIRECTORY_SEPARATOR . $path_to_file;
 }
 
 define('PATH_TO_LIB_FOR_PHP_UTILS', create_path(array(__DIR__, 'lib')));
