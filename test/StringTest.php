d7741518f634dacbea94382828f29ea01a9cf3e3
ttokutake <tadatoshi.tokutake@gmail.com>
2015-04-25 01:54:14 +0900

    Add start_with() and end_with()

--- test/StringTest.php
+++ test/StringTest.php
@@ -4,27 +4,70 @@ require_once implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'php-utils.php']);
 
 class StringTest extends PHPUnit_Framework_TestCase
 {
+   private $platitude    = 'hello, world!';
+   private $mb_platitude = 'こんにちは、世界！';
+
    public function testSplitAt()
    {
-      $platitude = 'hello, world!';
       $patterns = [
-         [['', 'hello, world!'], $platitude, -14],
-         [['', 'hello, world!'], $platitude, -13],
-         [['h', 'ello, world!'], $platitude, -12],
-         [['hello, ', 'world!'], $platitude, - 6],
-         [['hello, world', '!'], $platitude, - 1],
-         [['', 'hello, world!'], $platitude,   0],
-         [['h', 'ello, world!'], $platitude,   1],
-         [['hello, ', 'world!'], $platitude,   7],
-         [['hello, world', '!'], $platitude,  12],
-         [['hello, world!', ''], $platitude,  13],
-         [['hello, world!', ''], $platitude,  14],
+         [['', 'hello, world!'], -14],
+         [['', 'hello, world!'], -13],
+         [['h', 'ello, world!'], -12],
+         [['hello, ', 'world!'], - 6],
+         [['hello, world', '!'], - 1],
+         [['', 'hello, world!'],   0],
+         [['h', 'ello, world!'],   1],
+         [['hello, ', 'world!'],   7],
+         [['hello, world', '!'],  12],
+         [['hello, world!', ''],  13],
+         [['hello, world!', ''],  14],
       ];
-      foreach ($patterns as list($expected, $str, $at)) {
-         $this->assertEquals($expected, split_at($str, $at));
+      foreach ($patterns as list($expected, $at)) {
+         $this->assertEquals($expected, split_at($this->platitude, $at));
       }
+      $mb_patterns = [
+         [['こ', 'んにちは、世界！'], 1],
+         [['こんにちは、', '世界！'], 6],
+         [['こんにちは、世界', '！'], 8],
+      ];
+      foreach ($mb_patterns as list($expected, $at)) {
+         $this->assertEquals($expected, split_at($this->mb_platitude, $at));
+      }
+   }
 
-      $mb_str = 'こんにちは、世界！';
-      $this->assertEquals(['こんにちは、', '世界！'], split_at($mb_str, 6));
+   public function testStartWith()
+   {
+      $patterns = [
+         [true , 'hello'],
+         [false, 'ello,'],
+      ];
+      foreach ($patterns as list($expected, $needle)) {
+         $this->assertEquals($expected, start_with($this->platitude, $needle));
+      }
+      $mb_patterns = [
+         [true , 'こんにちは'],
+         [false, 'んにちは、'],
+      ];
+      foreach ($mb_patterns as list($expected, $needle)) {
+         $this->assertEquals($expected, start_with($this->mb_platitude, $needle));
+      }
+   }
+
+   public function testEndWith()
+   {
+      $patterns = [
+         [true , 'world!'],
+         [false, ' world'],
+      ];
+      foreach ($patterns as list($expected, $needle)) {
+         $this->assertEquals($expected, end_with($this->platitude, $needle));
+      }
+      $mb_patterns = [
+         [true , '世界！'],
+         [false, '、世界'],
+      ];
+      foreach ($mb_patterns as list($expected, $needle)) {
+         $this->assertEquals($expected, end_with($this->mb_platitude, $needle));
+      }
    }
 }
