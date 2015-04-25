d6714f4fe9882c42a007e003791a610bd8d42328
ttokutake <tadatoshi.tokutake@gmail.com>
2015-04-25 16:56:19 +0900

    Create is_blank()

--- test/StringTest.php
+++ test/StringTest.php
@@ -7,33 +7,6 @@ class StringTest extends PHPUnit_Framework_TestCase
    private $platitude    = 'hello, world!';
    private $mb_platitude = 'こんにちは、世界！';
 
-   public function testSplitAt()
-   {
-      $patterns = [
-         [['', 'hello, world!'], -14],
-         [['', 'hello, world!'], -13],
-         [['h', 'ello, world!'], -12],
-         [['hello, ', 'world!'], - 6],
-         [['hello, world', '!'], - 1],
-         [['', 'hello, world!'],   0],
-         [['h', 'ello, world!'],   1],
-         [['hello, ', 'world!'],   7],
-         [['hello, world', '!'],  12],
-         [['hello, world!', ''],  13],
-         [['hello, world!', ''],  14],
-      ];
-      foreach ($patterns as list($expected, $at)) {
-         $this->assertEquals($expected, split_at($this->platitude, $at));
-      }
-      $mb_patterns = [
-         [['こ', 'んにちは、世界！'], 1],
-         [['こんにちは、', '世界！'], 6],
-         [['こんにちは、世界', '！'], 8],
-      ];
-      foreach ($mb_patterns as list($expected, $at)) {
-         $this->assertEquals($expected, split_at($this->mb_platitude, $at));
-      }
-   }
 
    public function testStartWith()
    {
@@ -68,6 +41,63 @@ class StringTest extends PHPUnit_Framework_TestCase
       ];
       foreach ($mb_patterns as list($expected, $needle)) {
          $this->assertEquals($expected, end_with($this->mb_platitude, $needle));
+      }
+   }
+
+
+   public function testSplitAt()
+   {
+      $patterns = [
+         [['', 'hello, world!'], -14],
+         [['', 'hello, world!'], -13],
+         [['h', 'ello, world!'], -12],
+         [['hello, ', 'world!'], - 6],
+         [['hello, world', '!'], - 1],
+         [['', 'hello, world!'],   0],
+         [['h', 'ello, world!'],   1],
+         [['hello, ', 'world!'],   7],
+         [['hello, world', '!'],  12],
+         [['hello, world!', ''],  13],
+         [['hello, world!', ''],  14],
+      ];
+      foreach ($patterns as list($expected, $at)) {
+         $this->assertEquals($expected, split_at($this->platitude, $at));
+      }
+      $mb_patterns = [
+         [['こ', 'んにちは、世界！'], 1],
+         [['こんにちは、', '世界！'], 6],
+         [['こんにちは、世界', '！'], 8],
+      ];
+      foreach ($mb_patterns as list($expected, $at)) {
+         $this->assertEquals($expected, split_at($this->mb_platitude, $at));
+      }
+   }
+
+
+   public function testIsBlank()
+   {
+      $true_patterns = [
+         ''    ,
+         ' '   ,
+         "\t"  ,
+         //"\v"  ,
+         "\r"  ,
+         "\n"  ,
+         "\r\n",
+         "\f"  ,
+         //"　"  ,
+      ];
+      foreach ($true_patterns as $string) {
+         $this->assertTrue(is_blank($string));
+      }
+      $false_patterns = [
+         '1' ,
+         'a' ,
+         'A' ,
+         'あ',
+      ];
+      foreach ($false_patterns as $string) {
+         $this->assertFalse(is_blank($string));
       }
    }
 }
