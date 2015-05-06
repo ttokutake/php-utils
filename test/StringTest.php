e7a3a4cb001286943df6c33b74b1dc6005ca0fdf
ttokutake <tadatoshi.tokutake@gmail.com>
2015-05-06 20:18:52 +0900

    Refactor the test of string.inc and Rename set_encoding() => set_jp_encoding()

--- test/StringTest.php
+++ test/StringTest.php
@@ -2,7 +2,7 @@
 
 require_once implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'php-utils.php']);
 
-set_encoding();
+set_jp_encoding();
 
 class StringTest extends PHPUnit_Framework_TestCase
 {
@@ -26,44 +26,35 @@ class StringTest extends PHPUnit_Framework_TestCase
 
    public function testFollowJoin()
    {
-      $eol = PHP_EOL;
-      $this->assertEquals("{$this->platitude}$eol{$this->mb_platitude}$eol", follow_join($eol, [$this->platitude, $this->mb_platitude]));
+      $eol      = PHP_EOL;
+      $expected = "{$this->platitude}$eol{$this->mb_platitude}$eol";
+      $this->assertEquals($expected, follow_join($eol, [$this->platitude, $this->mb_platitude]));
    }
 
 
    public function testStartWith()
    {
       $patterns = [
-         [true , 'hello'],
-         [false, 'ello,'],
+         [true , $this->platitude   , 'hello'     ],
+         [false, $this->platitude   , 'ello,'     ],
+         [true , $this->mb_platitude, 'こんにちは'],
+         [false, $this->mb_platitude, 'んにちは、'],
       ];
-      foreach ($patterns as list($expected, $needle)) {
-         $this->assertEquals($expected, start_with($this->platitude, $needle));
-      }
-      $mb_patterns = [
-         [true , 'こんにちは'],
-         [false, 'んにちは、'],
-      ];
-      foreach ($mb_patterns as list($expected, $needle)) {
-         $this->assertEquals($expected, start_with($this->mb_platitude, $needle));
+      foreach ($patterns as list($expected, $haystack, $needle)) {
+         $this->assertEquals($expected, start_with($haystack, $needle));
       }
    }
 
    public function testEndWith()
    {
       $patterns = [
-         [true , 'world!'],
-         [false, ' world'],
+         [true , $this->platitude   , 'world!'],
+         [false, $this->platitude   , ' world'],
+         [true , $this->mb_platitude, '世界！'],
+         [false, $this->mb_platitude, '、世界'],
       ];
-      foreach ($patterns as list($expected, $needle)) {
-         $this->assertEquals($expected, end_with($this->platitude, $needle));
-      }
-      $mb_patterns = [
-         [true , '世界！'],
-         [false, '、世界'],
-      ];
-      foreach ($mb_patterns as list($expected, $needle)) {
-         $this->assertEquals($expected, end_with($this->mb_platitude, $needle));
+      foreach ($patterns as list($expected, $haystack, $needle)) {
+         $this->assertEquals($expected, end_with($haystack, $needle));
       }
    }
 
@@ -74,11 +65,9 @@ class StringTest extends PHPUnit_Framework_TestCase
          [['', 'hello, world!'], -14],
          [['', 'hello, world!'], -13],
          [['h', 'ello, world!'], -12],
-         [['hello, ', 'world!'], - 6],
          [['hello, world', '!'], - 1],
          [['', 'hello, world!'],   0],
          [['h', 'ello, world!'],   1],
-         [['hello, ', 'world!'],   7],
          [['hello, world', '!'],  12],
          [['hello, world!', ''],  13],
          [['hello, world!', ''],  14],
@@ -87,9 +76,15 @@ class StringTest extends PHPUnit_Framework_TestCase
          $this->assertEquals($expected, split_at($this->platitude, $at));
       }
       $mb_patterns = [
-         [['こ', 'んにちは、世界！'], 1],
-         [['こんにちは、', '世界！'], 6],
-         [['こんにちは、世界', '！'], 8],
+         [['', 'こんにちは、世界！'], -10],
+         [['', 'こんにちは、世界！'], - 9],
+         [['こ', 'んにちは、世界！'], - 8],
+         [['こんにちは、世界', '！'], - 1],
+         [['', 'こんにちは、世界！'],   0],
+         [['こ', 'んにちは、世界！'],   1],
+         [['こんにちは、世界', '！'],   8],
+         [['こんにちは、世界！', ''],   9],
+         [['こんにちは、世界！', ''],  10],
       ];
       foreach ($mb_patterns as list($expected, $at)) {
          $this->assertEquals($expected, split_at($this->mb_platitude, $at));
@@ -173,5 +168,6 @@ class StringTest extends PHPUnit_Framework_TestCase
    {
       $this->assertFalse(include_mb($this->platitude   ));
       $this->assertTrue (include_mb($this->mb_platitude));
+      $this->assertTrue (include_mb('JAM パターン'     ));
    }
 }
